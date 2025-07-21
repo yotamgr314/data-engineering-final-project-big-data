764fd5dd2b935419c1cd69267496c3d1f8d29413
shani-gal-or <shangalor@gmail.com>
2025-07-21 21:05:45 +0300

    last minute fixes
    mount path fixed to work in every machine
    added mount pathe to docker-compose of airflow
    try to fix unreconized path to spark (in docker-compose) - not fixed

--- processing/jobs/validate_bronze.py
+++ processing/jobs/validate_bronze.py
@@ -29,4 +29,18 @@ for tbl, cols in expected.items():
 if errors:
     for e in errors: print("❌", e)
     sys.exit(1)
+
+expected_tables = [
+    "bronze_boarding_events_raw",
+    "bronze_booked_tickets_raw_streaming",
+    "bronze_flight_events_raw",
+    "bronze_flight_weather_raw_api",
+    "bronze_flights_streaming.source",
+    "bronze_registered_customers_streaming",
+    "bronze_route_weather_points_static",
+    "bronze_static_routes_raw",
+    "bronze_ticket_events_raw_streaming",
+    "bronze_ticket_prices"
+]
+
 print("✔ Bronze schema validation passed.")
